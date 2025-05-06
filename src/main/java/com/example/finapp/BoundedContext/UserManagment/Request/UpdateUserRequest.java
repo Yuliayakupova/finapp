40eb3bbe976762a120cc1ad2ea9d0a424994b4de
Yuliayakupova <yuliayakupova@gmail.com>
2025-05-06 17:43:54 +0300

    add JavaDoc and comments for User and Transaction  modules

--- src/main/java/com/example/finapp/BoundedContext/UserManagment/Request/UpdateUserRequest.java
+++ src/main/java/com/example/finapp/BoundedContext/UserManagment/Request/UpdateUserRequest.java
@@ -1,11 +1,23 @@
 package com.example.finapp.BoundedContext.UserManagment.Request;
 
+/**
+ * Request object for updating an existing user.
+ * Contains the fields that can be modified when updating a user.
+ */
 public class UpdateUserRequest {
     private String email;
     private String password;
     private String name;
     private boolean isAuthenticated;
 
+    /**
+     * Constructs an UpdateUserRequest with the specified user details.
+     *
+     * @param email the user's email
+     * @param password the user's password
+     * @param name the user's name
+     * @param isAuthenticated indicates whether the user is authenticated
+     */
     public UpdateUserRequest(String email, String password, String name, boolean isAuthenticated) {
         this.email = email;
         this.password = password;
@@ -13,34 +25,74 @@ public class UpdateUserRequest {
         this.isAuthenticated = isAuthenticated;
     }
 
+    /**
+     * Gets the user's email.
+     *
+     * @return the email of the user
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Sets the user's email.
+     *
+     * @param email the new email of the user
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Gets the user's password.
+     *
+     * @return the password of the user
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Sets the user's password.
+     *
+     * @param password the new password of the user
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * Gets the user's name.
+     *
+     * @return the name of the user
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the user's name.
+     *
+     * @param name the new name of the user
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Checks if the user is authenticated.
+     *
+     * @return true if the user is authenticated, false otherwise
+     */
     public boolean isAuthenticated() {
         return isAuthenticated;
     }
 
+    /**
+     * Sets the authentication status of the user.
+     *
+     * @param authenticated the new authentication status of the user
+     */
     public void setAuthenticated(boolean authenticated) {
         isAuthenticated = authenticated;
     }
