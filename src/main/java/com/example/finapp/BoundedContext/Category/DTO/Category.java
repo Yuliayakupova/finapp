471179a33edeb6615e41f70859dcb1d076052da1
Yuliayakupova <yuliayakupova@gmail.com>
2025-05-06 17:08:28 +0300

    add JavaDoc and comments for Category modules

--- src/main/java/com/example/finapp/BoundedContext/Category/DTO/Category.java
+++ src/main/java/com/example/finapp/BoundedContext/Category/DTO/Category.java
@@ -1,36 +1,83 @@
 package com.example.finapp.BoundedContext.Category.DTO;
 
+/**
+ * The Category class represents a category in the personal finance application.
+ * It holds details about the category such as its unique identifier (id), name, and type.
+ */
 public class Category {
+
+    // The unique identifier for the category.
     private int id;
+
+    // The name of the category (e.g., "Food", "Entertainment").
     private String name;
+
+    // The type of the category (e.g., "Income", "Expense").
     private String type;
 
+    /**
+     * Constructor for creating a new Category object with specified id, name, and type.
+     *
+     * @param id The unique identifier of the category.
+     * @param name The name of the category.
+     * @param type The type of the category.
+     */
     public Category(int id, String name, String type) {
         this.id = id;
         this.name = name;
         this.type = type;
     }
 
+    /**
+     * Gets the unique identifier of the category.
+     *
+     * @return The unique id of the category.
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Sets the unique identifier of the category.
+     *
+     * @param id The unique identifier to set for the category.
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Gets the name of the category.
+     *
+     * @return The name of the category.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the category.
+     *
+     * @param name The name to set for the category.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets the type of the category.
+     *
+     * @return The type of the category (e.g., "Income" or "Expense").
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * Sets the type of the category.
+     *
+     * @param type The type to set for the category.
+     */
     public void setType(String type) {
         this.type = type;
     }
--- src/main/java/com/example/finapp/BoundedContext/Category/Request/UpdateCategoryRequest.java
+++ src/main/java/com/example/finapp/BoundedContext/Category/Request/UpdateCategoryRequest.java
@@ -1,26 +1,57 @@
 package com.example.finapp.BoundedContext.Category.Request;
 
+/**
+ * The UpdateCategoryRequest class is used to encapsulate the data required to update an existing category.
+ * It contains the name and type of the category to be updated.
+ */
 public class UpdateCategoryRequest {
-    private String name;
-    private String type;
 
+    private String name;  // The new name of the category to be updated
+    private String type;  // The new type of the category to be updated
+
+    /**
+     * Constructs a new UpdateCategoryRequest with the specified name and type.
+     *
+     * @param name the new name of the category.
+     * @param type the new type of the category.
+     */
     public UpdateCategoryRequest(String name, String type) {
         this.name = name;
         this.type = type;
     }
 
+    /**
+     * Gets the name of the category.
+     *
+     * @return the name of the category.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the category.
+     *
+     * @param name the new name of the category.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets the type of the category.
+     *
+     * @return the type of the category.
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * Sets the type of the category.
+     *
+     * @param type the new type of the category.
+     */
     public void setType(String type) {
         this.type = type;
     }
